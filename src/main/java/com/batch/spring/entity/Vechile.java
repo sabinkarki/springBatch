884f8d54f8746ddebf96b38ca8dd3b99a888a581
Sabin Karki <skarki@ddmi.intra.renhsc.com>
2018-08-02 21:40:28 -0400

    Added Vechile.java

---
+++ src/main/java/com/batch/spring/entity/Vechile.java
@@ -0,0 +1,109 @@
+package com.batch.spring.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Vechile")
+public class Vechile {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "Id", nullable = false)
+	private long id;
+	private String type;
+	private String model;
+	private String built;
+
+	public Vechile() {
+	}
+
+	public Vechile(long id, String type, String model, String built) {
+		super();
+		this.type = type;
+		this.model = model;
+		this.built = built;
+		this.id = id;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getBuilt() {
+		return built;
+	}
+
+	public void setBuilt(String built) {
+		this.built = built;
+	}
+
+	@Override
+	public String toString() {
+		return "Vechile [id=" + id + ", type=" + type + ", model=" + model + ", built=" + built + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((built == null) ? 0 : built.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((model == null) ? 0 : model.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vechile other = (Vechile) obj;
+		if (built == null) {
+			if (other.built != null)
+				return false;
+		} else if (!built.equals(other.built))
+			return false;
+		if (id != other.id)
+			return false;
+		if (model == null) {
+			if (other.model != null)
+				return false;
+		} else if (!model.equals(other.model))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+}

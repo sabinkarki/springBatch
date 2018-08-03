a3ea13ebf1bb7472cff08fed7d7cc0b1cd9ead6c
Sabin Karki <skarki@ddmi.intra.renhsc.com>
2018-08-02 21:36:47 -0400

    Added Home RestController and Runner.java

---
+++ src/main/java/com/batch/spring/BatchSpringRunner.java
@@ -0,0 +1,13 @@
+package com.batch.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BatchSpringRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchSpringRunner.class, args);
+	}
+}

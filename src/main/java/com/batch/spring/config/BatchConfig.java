dac614e2e923dd8fb4b4bc04ef40364fbe4e9706
sabin <nibaserc@gmail.com>
2018-08-12 16:07:58 -0400

    Refactored to inject vechileprocessor,vechilewriter,wordreader,wordprocessor and wordwriter and removed JdbcBatchItemWriter

--- src/main/java/com/batch/spring/config/BatchConfig.java
+++ src/main/java/com/batch/spring/config/BatchConfig.java
@@ -1,15 +1,10 @@
 package com.batch.spring.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
-import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
@@ -24,6 +19,7 @@ import com.batch.spring.steps.listener.JobCompletionNotificationListner2;
 import com.batch.spring.steps.processor.VechileProcessor;
 import com.batch.spring.steps.processor.WordProcessor;
 import com.batch.spring.steps.reader.WordReader;
+import com.batch.spring.steps.writer.VechileWriter;
 import com.batch.spring.steps.writer.WordWriter;
 
 @Configuration
@@ -36,9 +32,20 @@ public class BatchConfig {
 	private StepBuilderFactory stepBuilderFactory;
 	
 	@Autowired
-	private DataSource dataSource;
+	private VechileProcessor vechileProcessor;
 	
-	//start :: jobstep[]
+	@Autowired
+	private VechileWriter vechileWriter;
+	
+	@Autowired
+	private WordReader wordReader;
+	
+	@Autowired
+	private WordProcessor wordProcessor;
+	
+	@Autowired
+	private WordWriter wordWriter;
+	
 	
 	@Bean(name ="job1")
 	public Job job1(JobCompletionNotificationListener1 listner){
@@ -56,8 +63,7 @@ public class BatchConfig {
 		return jobs.get("job2")
 				.incrementer(new RunIdIncrementer())
 				.listener(listner)
-				.flow(step2())
-				.end()
+				.start(step2())   //for only one step
 				.build();
 	}
 	
@@ -66,8 +72,8 @@ public class BatchConfig {
 		return stepBuilderFactory.get("step1")
 				.<Vechile,Vechile>chunk(10)
 				.reader(reader())
-				.processor(processor())
-				.writer(writer())
+				.processor(vechileProcessor)
+				.writer(vechileWriter)
 				.build();
 	}
 	
@@ -75,18 +81,18 @@ public class BatchConfig {
 	public Step step2(){
 		return stepBuilderFactory.get("step2")
 				.<String, String> chunk(1)
-				.reader(new WordReader())
-				.processor(new WordProcessor())
-				.writer(new WordWriter())
+				.reader(wordReader)
+				.processor(wordProcessor)
+				.writer(wordWriter)
 				.build();
 	}
 
-	//end :jobstep
 	
 	@Bean
 	public FlatFileItemReader<Vechile> reader() {
 		return new FlatFileItemReaderBuilder<Vechile>().name("vechileItemReader").resource(new ClassPathResource("vechiles.csv"))
-				.delimited().names(new String[] { "id", "type","model","built" })
+				.delimited().names(new String[] {"type","model","built" })
+				.linesToSkip(1)  //skipping row one from csv file
 				.fieldSetMapper(new BeanWrapperFieldSetMapper<Vechile>(){
 					
 					{
@@ -94,21 +100,5 @@ public class BatchConfig {
 					}
 				}).build();
 	}
-	
-	@Bean
-	public VechileProcessor processor(){
-		return new VechileProcessor();
-	}
-	
-	
-	
-	@Bean
-	public JdbcBatchItemWriter<Vechile> writer() {
-		return new JdbcBatchItemWriterBuilder<Vechile>()
-				.itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
-				.sql("INSERT INTO Vechile (id,type,model,built) VALUES (:id,:type,:model,:built)").dataSource(this.dataSource)
-				.build();
-	}
-	
-	
+			
 }

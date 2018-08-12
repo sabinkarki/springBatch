9574ef0e5d575786d01dad44d5da19187889e2a4
sabin <nibaserc@gmail.com>
2018-08-12 16:00:15 -0400

    Refactored to add id

--- src/main/java/com/batch/spring/steps/processor/VechileProcessor.java
+++ src/main/java/com/batch/spring/steps/processor/VechileProcessor.java
@@ -1,17 +1,20 @@
  package com.batch.spring.steps.processor;
 
+import org.springframework.stereotype.Component;
+
 import com.batch.spring.entity.Vechile;
 
-
+@Component
 public class VechileProcessor implements org.springframework.batch.item.ItemProcessor<Vechile, Vechile> {
 	
+	private  static long id =0;
 	
 	@Override
 	public Vechile process(Vechile vechile) throws Exception {
 		
-		if(Integer.parseInt(vechile.getBuilt())<1998){
+		if(Integer.parseInt(vechile.getBuilt())>1998){
 			final String model = firstIndexCapital(vechile.getModel()).toString();
-			vechile = new Vechile(vechile.getId(),vechile.getType(),model,vechile.getBuilt());
+			vechile = new Vechile(++id,vechile.getType(),model,vechile.getBuilt());
 		}
 		
 		return vechile;
